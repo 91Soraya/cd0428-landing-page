160cf9740c5f43ddacaf7e8cf2c28eee58cefd37
Soraya de Ruiter <sorayamr91@gmail.com>
2022-09-07 16:48:41 +0200

    your active class function added

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@
 */
 const navMenu = document.querySelector(".navbar__menu");
 const navList = document.querySelector("#navbar__list");
-const navSections = document.querySelectorAll("section");
+const sections = document.querySelectorAll("section");
 
 /**
  * End Global Variables
@@ -41,26 +41,66 @@ const navSections = document.querySelectorAll("section");
 */
 
 // build the nav
-for (let i=0; i < navSections.length; i++) {
+for (let i=0; i < sections.length; i++) {
     const createNavListItem = document.createElement("li");
     const createNavAnchor = document.createElement("a");
-    createNavAnchor.innerHTML = navSections[i].getAttribute("data-nav");
-    createNavAnchor.href = "#" + navSections[i].getAttribute("id");
+    createNavAnchor.innerHTML = sections[i].getAttribute("data-nav");
+    createNavAnchor.href = "#" + sections[i].getAttribute("id");
+    createNavAnchor.classList = "navLink";
+    createNavAnchor.id = "linkToSection" + [i+1];
     createNavListItem.appendChild(createNavAnchor);
     navList.appendChild(createNavListItem);
 }
 
 // Add class 'active' to section when near top of viewport
-const activeSection = navSections[0].getBoundingClientRect();
-window.addEventListener("scroll", function () {
-    // console.log(window.innerHeight);
-    // console.log(sectionOne.getBoundingClientRect().top);
-    if (activeSection.getBoundingClientRect().top < window.innerHeight) {
-        section.classList.add("your-active-class");
-    } else {
-        section.classList.remove("your-active-class");
-    }
-});
+
+// const sectionThree = document.querySelector("#section3");
+
+// document.addEventListener("scroll", function() {
+//     const clientHeight = document.documentElement.clientHeight;
+//     const sectionThreeY = sectionThree.getBoundingClientRect().y;
+//     const sectionThreeBottom = sectionThree.getBoundingClientRect().bottom;
+
+//     // console.log(clientHeight);
+//     // console.log(sectionThreeY);
+//     // console.log(sectionThreeBottom);
+
+//     if(sectionThreeY <= 0 && sectionThreeBottom > 0) {
+//         sectionThree.classList.add("your-active-class");
+//     } else {
+//         sectionThree.classList.remove("your-active-class");
+//     }
+// })
+
+document.addEventListener("scroll", function () {
+    sections.forEach(function (section) {
+        if(section.getBoundingClientRect().y <= 0 && section.getBoundingClientRect().bottom >0) {
+            section.classList.add("your-active-class");
+            // console.log("active class added");
+        } else {
+            section.classList.remove("your-active-class");
+            // console.log("active class removed");
+        }
+        });
+})
+
+// function navHighlight() {
+//     const navA = document.querySelectorAll("a");
+//     sections.forEach(function (section) {
+//         if(section.classList == "your-active-class") {
+//             navA.classList.add("active");
+//         } else {
+//             navA.classList.remove("active");
+//         }
+//     })
+// }
+
+// document.addEventListener("scroll", navHighlight);
+
+
+// const rect = sections.getBoundingClientRect();
+// console.log(rect);
+
 // Scroll to anchor ID using scrollTO event
 
 
@@ -75,5 +115,3 @@ window.addEventListener("scroll", function () {
 // Scroll to section on link click
 
 // Set sections as active
-
-
